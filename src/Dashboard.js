f6e1e88b2a7c6292ec571ee652080c353b0fd4cd
Alyssa Hope <alyssamhope@gmail.com>
2018-07-09 15:17:50 -0700

    adds table view to Dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
-import {Grid, Row, Col} from 'react-bootstrap'
-import neoData from './example-api-response.json'
+import {Grid, Row, Col, Jumbotron} from 'react-bootstrap'
+import apiData from './example-api-response'
 
 class Dashboard extends Component {
-  render() {
-    return (
+	constructor(props) {
+		super(props)
+		this.state = {
+			neoData: apiData.near_earth_objects
+		}
+	}
+
+	render() {
+		// find the array/date
+
+		// map over each item in the array
+		let near_earth_objects = this.state.neoData.map(function(element){
+			console.log(element.is_potentially_hazardous_asteroid);
+			return (
+				<tr className="table-success">
+				  <td>{element.name}</td>
+				  <td>{element.is_potentially_hazardous_asteroid.toString()}</td>
+				  <td>{element.close_approach_data[0].relative_velocity.miles_per_hour}</td>
+				  <td>{element.close_approach_data[0].miss_distance.miles}</td>
+				</tr>
+			)
+		})
+
+		return (
 		<Grid>
 			<Row>
 				<Col xs={12}>
-					<div className="jumbotron">
+					<Jumbotron>
 						<h1 className="display-3">Nasa NEO</h1>
 						<p className="lead">
 							This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
@@ -22,8 +44,24 @@ class Dashboard extends Component {
 								Learn more
 							</a>
 						</p>
-					</div>
+					</Jumbotron>
 				</Col>
+			</Row>
+			<Row>
+				<h3>Near Earth Objects for [date]</h3>
+				<table class="table table-hover">
+					<thead>
+						<tr>
+						  <th scope="col">Name</th>
+						  <th scope="col">Hazardous?</th>
+						  <th scope="col">Relative MPH</th>
+						  <th scope="col">Miss Distance</th>
+						</tr>
+					</thead>
+					<tbody>
+						{near_earth_objects}
+					</tbody>
+				</table>
 			</Row>
 		</Grid>
     );
